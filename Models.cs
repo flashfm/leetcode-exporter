5477155fbbb2b999401df34a496d5e6d92db15f1
flashfm <alex.flashfm@gmail.com>
2025-02-14 12:22:46 -0800

    feat: structuring improvements

--- Models.cs
+++ Models.cs
@@ -8,26 +8,29 @@ public class SubmissionsResponse
     public bool HasNext { get; set; }
 
     [JsonPropertyName("submissions_dump")]
-    public IReadOnlyList<Submission> Submissions { get; set; } = Array.Empty<Submission>();
+    public IReadOnlyList<Submission> Submissions { get; set; } = [];
 }
 
 public class Submission
 {
-    public string Lang { get; set; } = null!;
+    public string Lang { get; set; } = "";
     
     public int Timestamp { get; set; }
     
     [JsonPropertyName("status_display")]
-    public string Status { get; set; } = null!;
+    public string Status { get; set; } = "";
+
+    [JsonPropertyName("question_id")]
+    public int QuestionId { get; set; }
     
-    public string Runtime { get; set; } = null!;
+    public string Runtime { get; set; } = "";
     
-    public string Title { get; set; } = null!;
+    public string Title { get; set; } = "";
     
-    public string Memory { get; set; } = null!;
+    public string Memory { get; set; } = "";
 
     [JsonPropertyName("title_slug")]
-    public string TitleSlug { get; set; } = null!;
+    public string TitleSlug { get; set; } = "";
     
-    public string Code { get; set; } = null!;
+    public string Code { get; set; } = "";
 }
--- Program.cs
+++ Program.cs
@@ -7,19 +7,20 @@ public class Program
 {
     private const string CookieName = "LEETCODE_SESSION";
     private const string Host = "leetcode.com";
-    private static readonly TimeSpan DelayBetweenRequests = TimeSpan.FromSeconds(2);
+    private const string dataFileName = "data.json";
+
+    private static readonly TimeSpan DelayBetweenRequests = TimeSpan.FromSeconds(10);
     private static readonly JsonSerializerOptions JsonSerializerOptions = new() {
         PropertyNamingPolicy = JsonNamingPolicy.CamelCase
     };
 
-    private readonly List<Submission> allSubmissions = new();
+    public static async Task Main()
+        => await new Program().Run();
 
-    public static async Task Main() => await new Program().Run();
-    
     private async Task Run()
     {
         await Download();
-        await Save();
+        await Extract();
     }
 
     private async Task Download()
@@ -37,66 +38,73 @@ public class Program
         });
         const int limit = 20;
         var offset = 0;
-        SubmissionsResponse response;
+        var submissions = new List<Submission>();
         while (true) {
-            Console.Write(".");
+            Console.WriteLine($"Offset {offset}");
             await using var responseStream =
                 await client.GetStreamAsync($"https://{Host}/api/submissions/?offset={offset}&limit={limit}");
-            response = JsonSerializer.Deserialize<SubmissionsResponse>(responseStream, JsonSerializerOptions) ??
+            var response = JsonSerializer.Deserialize<SubmissionsResponse>(responseStream, JsonSerializerOptions) ??
                        throw new InvalidOperationException("Invalid response.");
-            allSubmissions.AddRange(response.Submissions);
+            submissions.AddRange(response.Submissions);
             if (!response.HasNext) {
                 break;
             }
             offset += limit;
             await Task.Delay(DelayBetweenRequests);
         }
-        Console.WriteLine();        
+        Console.WriteLine($"Saving to {dataFileName}...");
+        await using var dataFile = File.CreateText(dataFileName);
+        await dataFile.WriteAsync(JsonSerializer.Serialize(submissions));
+        Console.WriteLine();
     }
 
-    private async Task Save()
+    private async Task Extract()
     {
+        var allSubmissions = JsonSerializer.Deserialize<IReadOnlyList<Submission>>(await File.ReadAllTextAsync(dataFileName))
+            ?? throw new InvalidOperationException("Error deserializing.");
+
         var dir = $"leetcode-export-{DateTime.Now:yyyy-MM-dd}";
         Console.WriteLine($"Saving to ./{dir}/ ...");
-        
+
         if (!Directory.Exists(dir)) {
             Directory.CreateDirectory(dir);
         }
 
         var acceptedSubmissions = allSubmissions
             .Where(s => s.Status == "Accepted")
-            .GroupBy(s => s.TitleSlug)
-            .Select(g => g.Select(s => new {
-                Submission = s,
-                RuntimeMillisecond = ParseMetric(s.Runtime, "ms"),
-                MemoryMb = ParseMetric(s.Memory, "MB")
-            }).OrderBy(s => s.RuntimeMillisecond).ThenBy(s => s.MemoryMb).Select(t => t.Submission).First());
-        
-        foreach (var submission in acceptedSubmissions) {
-            await using var file = File.CreateText($"./{dir}/{submission.TitleSlug}.{GetExtension(submission)}");
-            file.WriteLine($"// {submission.Title}");
-            file.WriteLine($"// https://{Host}/problems/{submission.TitleSlug}");
-            file.WriteLine($"// Date solved: {DateTimeOffset.FromUnixTimeSeconds(submission.Timestamp)}");
-            file.WriteLine();
-            file.WriteLine(submission.Code);
-        }        
+            .GroupBy(s => (s.QuestionId, s.Title))
+            .Select(g => new {
+                QuestionId = g.Key.QuestionId,
+                Title = g.Key.Title,
+                ProblemSubmissions = g
+                    .OrderBy(s => s.Timestamp)
+            });
+
+        foreach (var submissionGroup in acceptedSubmissions) {
+            var index = 1;
+            foreach(var submission in submissionGroup.ProblemSubmissions) {
+                var subdir = $"./{dir}/{submission.QuestionId} - {submission.Title}";
+                Directory.CreateDirectory(subdir);
+                var fileName = $"{subdir}/{index}.{GetExtension(submission)}";
+                var date = DateTimeOffset.FromUnixTimeSeconds(submission.Timestamp);
+                Console.WriteLine($"Saving to {fileName}");
+                await using var file = File.CreateText(fileName);
+                file.WriteLine($"// Copyright (c) {date.Year} Alexey Filatov");
+                file.WriteLine($"// {submission.QuestionId} - {submission.Title} (https://{Host}/problems/{submission.TitleSlug})");
+                file.WriteLine($"// Date solved: {date}");
+                file.WriteLine($"// Memory: {submission.Memory}");
+                file.WriteLine($"// Runtime: {submission.Runtime}");
+                file.WriteLine($"");
+                file.WriteLine();
+                file.WriteLine(submission.Code);
+                index++;
+            }
+        }
     }
 
-    private string GetExtension(Submission submission)
+    private static string GetExtension(Submission submission)
     {
-        var ext = submission.Lang;
-        if (ext == "csharp") {
-            ext = "cs";
-        }
-        return ext;
-    }
-
-    private decimal ParseMetric(string metric, string expectedMeasure)
-    {
-        var parts = metric.Split(' ');
-        if (parts.Length != 2 || parts[1] != expectedMeasure) {
-            throw new ArgumentException($"Error parsing metric value.", nameof(metric));
-        }
-        return decimal.Parse(parts[0]);
+        var lang = submission.Lang;
+        return lang == "csharp" ? "cs" : lang; 
     }
 }
